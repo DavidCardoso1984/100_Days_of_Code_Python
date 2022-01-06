b8a405a404ac4a734a4a71403824d09105d8fde7
decardoso <david.cardoso1984@gmail.com>
2022-01-06 14:35:36 -0300

    Adicionado a aula 29 e a pasta do Day_3 do curso.

---
+++ Day_3/Day_3_Aula_29.py
@@ -0,0 +1,32 @@
+# Aula 29 - Control Flow with if / else anda Conditional Operators.
+
+# Condicionais:
+# Exemplo do uso de válcula de transbordo de uma banheira;
+# if/else;
+
+# Exemplo:
+
+# if condition:
+#     do this 
+# else:
+#     do this
+
+# water level = 50
+# if water level > 80:
+#     print("Drain water") 
+# else:
+#     print("Continue")
+
+# Exemplo rollercoaster:
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+
+# Teste lógico básico, alguns pontos de atenção são não esquecer de 
+# usar : depois dos testes lógicos e cuidar com a identação, como já
+# mencionado no Python isso é muito importante.
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("You can't ride the rollercoaster!")
