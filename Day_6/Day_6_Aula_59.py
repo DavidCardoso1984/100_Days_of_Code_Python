6a63c8166f2640b8099931d5707eeae711418450
David Andre Cardoso <davidc@weg.net>
2022-01-21 12:10:57 -0300

    Adicioado a pasta do dia 6 do treinamento e o arquivo sa aula 59 deste dia.

---
+++ Day_6/Day_6_Aula_59.py
@@ -0,0 +1,31 @@
+# Day 6 - Aula 59 - Defining and Calling Python Functions
+
+# Aula sobre functions
+
+# Já vinhamos usando funções durante o curso.
+
+# Exemplos
+
+print("Hello!")
+
+len_char = len("Hello!")
+print(len_char)
+
+# Como usar o comando def para criar funções personalizadas:
+
+# Definindo uma função:
+def my_function():
+    print("Hello!")
+    print("Bye!")
+
+# Executando uma função:
+my_function()
+
+# Defining a function:
+# def my_function():
+#     #Do this
+#     #Then do this
+#     #Finally do this
+
+# Calling Functions:
+# my_function
