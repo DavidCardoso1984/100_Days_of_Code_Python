8e3c33afe50f66bc6582e4d05a9be894a050bf4b
decardoso <david.cardoso1984@gmail.com>
2022-01-26 21:08:51 -0300

    Adicionado as atividades da aula 60, a partir da aula 59.

---
+++ Day_6/Day_6_Aula_59_Robot.py
@@ -0,0 +1,27 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+move()
+turn_left()
+move()
+turn_left()
+move()
+turn_left()
+move()
+turn_left()
+move()
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
