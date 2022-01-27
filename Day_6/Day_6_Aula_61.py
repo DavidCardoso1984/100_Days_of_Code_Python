2c1b606213aee359862e2b176fbf26a5acd2c1c4
decardoso <david.cardoso1984@gmail.com>
2022-01-26 21:10:23 -0300

    Atualização das atividades do dia 6.

---
+++ Day_6/Day_6_Aula_61.py
@@ -0,0 +1,19 @@
+#Day 6 - Aula 61 - Indentation
+
+# Identação é um cuidado básico para manter um código legível.
+# No Python além de legibilidade ela tem a função de definir os blocos de código.
+
+
+# Exemplos de blocos de códigos:
+
+# def my_function():   #Dentro do bloco de código
+# ****print("Hello!")  #Dentro do bloco de código
+#print("Wolrd!")       #Fora do bloco de código  
+
+# def my_function():
+#     if sky == "clear":
+#         print("blue")
+#     elif sky == "cloudy":
+#         print("grey")
+#     print("Hello")
+# print("World!")
---
+++ Day_6/Day_6_Aula_62.py
@@ -0,0 +1,7 @@
+# Day 6 - Aula 62 - While Loop
+
+# while something_is_true:
+#     Do this
+#     Then do this
+#     Then do more this
+
