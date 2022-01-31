8921979a37729e59095cdaac16b7ad33071ac368
decardoso <david.cardoso1984@gmail.com>
2022-01-30 22:57:47 -0300

    Adicionado as aulas 68 e 69 do dia 7.

---
+++ Day_7/Day_7_Aula_69.py
@@ -0,0 +1,14 @@
+# Day 7 - Aula 69 - Picking a Random Words And Cheking Answer
+
+# Step 1:
+
+word_list = ["ardvark", "baboon", "camel"]
+
+# ToDo_1 - Randomly choose a word from the word_list and
+# assign it to a variable called chosen_word.
+
+# ToDo_2 - Ask the user to guess a letter and assign their
+# answer to a variable called guess. Make guess lowercase.
+
+# ToDo_3 - Check it the letter the user guessed (guess) is
+# one pf the leters in the chosen_word. 
